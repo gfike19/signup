b52ee8e1302a501a0dcb21cadc7eddd54c5574f6
gfike19 <gfike19@gmail.com>
2016-08-24 18:51:57 -0500

    got main almost working

--- main.py
+++ main.py
@@ -1,26 +1,95 @@
 import webapp2
+import cgi
+import re
+
+info_form = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+</head>
+<body>
+<form action = "input" method = "post">
+    <label>
+        Username:
+        <input type = "text" name = "uname"/>
+    </label>
+    <p>
+    <label>
+        Password:
+        <input type = "password" name = "pwd"/>
+    </label>
+    </p>
+    <p>
+    <label>
+        Verify Password:
+        <input type = "password" name = "vpwd"/>
+    </label>
+    </p>
+    <p>
+    <label>
+        Email:
+        <input type = "text" name = "email"/>
+    </label>
+    </p>
+    <input type = "submit"/>
+</form>
+</body>
+</html>
+"""
+error = ""
+#<div>%(error)s</div>
 
 class Index(webapp2.RequestHandler):
+
+
+
+    # def write_form(self):
+    #     self.response.out.write(info_form)
+
+        #%{"error": error})
+
     def get(self):
-        info_form = """
-        <form action = "input" method = post>
+        self.response.write(info_form)
 
-            <label>
-                Username:
-                <input = "text" name = "uname"/>
-            </label>
+    def post(self):
+        uname = str(self.request.get("uname"))
+        pwd = str(self.request.get("pwd"))
+        vpwd = str(self.request.get("vpwd"))
+        email = str(self.request.get("email"))
 
-            <label>
-                Password:
-                <input = "text" name = "pwd"/>
-            </label>
+        uname_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        def valid_uname(uname):
+            return uname_re.match(uname)
 
-            <label>
-                
-            </label>
+        pwd_re = re.compile(r"^.{3,20}$")
+        def valid_pwd(pwd):
+            return pwd_re.match(pwd)
 
-        </form>
-        """
+        email_re = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+        def valid_email(email):
+            return email_re.match(email)
+
+        check_uname = valid_uname(uname)
+        if check_uname == False:
+            error += "<br>Invalid username<br>"
+            # self.write_form("<br>Invalid username<br>")
+
+        check_pwd = valid_pwd(pwd)
+        if check_pwd == False:
+            error += "<br>Invalid password<br>"
+            # self.write_form("<br>Invalid password<br>")
+        if pwd != vpwd == False:
+            error += "<br>Passwords do not match<br>"
+            # self.write_form("<br>Passwords do not match<br>")
+
+        check_email = valid_email(email)
+        if check_email == False:
+            error += "<br>Invalid email address<br>"
+            # self.write_form("<br>Invalid email address<br>")
+
+    # self.write_form()
+        self.response.write(info_form + error)
 
 app = webapp2.WSGIApplication([
     ('/', Index)
